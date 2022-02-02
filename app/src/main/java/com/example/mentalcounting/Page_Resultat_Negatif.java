d660bdf627d2c5a5ddd6aa2955bd3bd85ec15fe5
BonbonRodrigue <omega6867@gmail.com>
2022-02-02 12:28:58 +0100

    rendu du projet'

---
+++ app/src/main/java/com/example/mentalcounting/Page_Resultat_Negatif.java
@@ -0,0 +1,14 @@
+package com.example.mentalcounting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page_Resultat_Negatif extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_resultat_negatif);
+    }
+}
